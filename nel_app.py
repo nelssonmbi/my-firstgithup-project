5d6494709989bd5366c877f6c90b8173f2cd5587
nelssonmbi <109183388+nelssonmbi@users.noreply.github.com>
2022-07-12 18:08:04 -0500

    Update nel_app.py

--- nel_app.py
+++ nel_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("my Wife new health Dinner")
-sttreamlit.header("Breakfast Manu")
+streamlit.header("Breakfast Manu")
 streamlit.text('omega 3 $ blueberry Oatmeal')
 streamlit.text('kale, Spinach $ rocket smothie')
 streamlit.text('Hard-Boiled Free Range Egg')
