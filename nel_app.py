dbc3bd57a38d1f058910f47093ece3a6cdb9213d
nelssonmbi <109183388+nelssonmbi@users.noreply.github.com>
2022-07-12 18:28:29 -0500

    Update nel_app.py

--- nel_app.py
+++ nel_app.py
@@ -4,6 +4,6 @@ streamlit.header("Breakfast Manu")
 streamlit.text('omega 3 $ blueberry Oatmeal')
 streamlit.text('kale, Spinach $ rocket smothie')
 streamlit.text('Hard-Boiled Free Range Egg')
-import pandas as pd
-my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+import pandas 
+my_fruit_list=Pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
